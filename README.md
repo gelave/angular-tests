d875241462d29b82e6b04f3ed2652fc0bbd5177e
Angela Vega <angela+sw@rokk3rlabs.com>
2018-10-11 15:54:20 -0500

    Submitting Module 1 Exercise

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# angular-tests
+# Coursera Angular Course
---
+++ module1-exercise/app.js
@@ -0,0 +1,53 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController)
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController ($scope) {
+	
+	$scope.colorAttribute = "gray";
+	
+	$scope.checkAmount = function () {
+
+		if($scope.lunchMenu) {
+			
+			var allListedItems = $scope.lunchMenu.split(',');
+			var emptyItems = 0;
+
+			for (var i = 0; i < allListedItems.length; i++) {
+		        if (allListedItems[i].trim() == "" ) {
+		            emptyItems++;
+		        }
+		    }
+
+		    var validItems = allListedItems.length - emptyItems;
+			console.log("valid items", validItems, allListedItems.length, emptyItems)
+			if (validItems == 0 || allListedItems.length == 0) {
+				$scope.message = "Please enter data first";
+				$scope.colorAttribute = "red";
+			} else if (validItems <=3) {
+				$scope.message = "Enjoy!";
+				$scope.colorAttribute = "green";
+			} else {
+				$scope.message = "Too much!";
+				$scope.colorAttribute = "green";
+			}
+			
+
+		} else {
+			$scope.message = "Please enter data first";
+			$scope.colorAttribute = "red";	
+		}
+		
+	}
+
+	$scope.clearMessage = function () {
+		$scope.message = "";	
+	}
+
+};
+
+
+})();
